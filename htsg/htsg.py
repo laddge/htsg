da2143f16d43b83c822aed0f3b5c3605c17faa95
Laddge <dev.laddge@gmail.com>
2022-05-09 12:44:44 +0000

    [feat] enhanced visual

--- htsg/htsg.py
+++ htsg/htsg.py
@@ -1,14 +1,42 @@
 import os
+import time
+import threading
 import shutil
 import tempfile
 import http.server
 import socketserver
 import functools
 import hashlib
+from contextlib import redirect_stdout
 import toml
 from jinja2 import Environment, FileSystemLoader
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
+
+
+class _spinner:
+    def __init__(self, prefix):
+        self.prefix = prefix
+        self.done = False
+
+    def _loop(self):
+        spinners = ["/", "-", "\\", "|"]
+        i = 0
+        while True:
+            if self.done:
+                break
+            sp = spinners[i]
+            print(f"\r{self.prefix} ... {sp}", end="")
+            i = (i + 1) % 4
+            time.sleep(.2)
+
+    def start(self):
+        self.done = False
+        threading.Thread(target=self._loop).start()
+
+    def stop(self):
+        self.done = True
+        print(f"\r{self.prefix} ... done.")
 
 
 def generate(
@@ -18,9 +46,20 @@ def generate(
     cfgfile="./config.toml",
     globals={},
 ):
+    print("---")
+    print(f"srcdir  = '{srcdir}'")
+    print(f"tpldir  = '{tpldir}'")
+    print(f"distdir = '{distdir}'")
+    print(f"cfgfile = '{cfgfile}'")
+    print("---")
     with tempfile.TemporaryDirectory() as tmp:
         tmpdir = os.path.join(tmp, "dist")
+        sp = _spinner("Copying static files")
+        sp.start()
         shutil.copytree(srcdir, tmpdir)
+        sp.stop()
+        sp = _spinner("Rendering")
+        sp.start()
         env = Environment(loader=FileSystemLoader(tpldir, encoding="utf8"))
         env.globals = globals
         with open(cfgfile) as f:
@@ -35,11 +74,15 @@ def generate(
             os.makedirs(os.path.dirname(path), exist_ok=True)
             with open(path, "w") as f:
                 f.write(tpl.render(params))
+        sp.stop()
+        sp = _spinner("Copying all files to distdir")
+        sp.start()
         if os.path.isfile(distdir):
             os.remove(distdir)
         if os.path.isdir(distdir):
             shutil.rmtree(distdir)
         shutil.copytree(tmpdir, distdir)
+        sp.stop()
 
 
 def serve(
@@ -51,6 +94,12 @@ def serve(
     cfgfile="./config.toml",
     globals={},
 ):
+    print("---")
+    print(f"srcdir  = '{srcdir}'")
+    print(f"tpldir  = '{tpldir}'")
+    print(f"distdir = '{distdir}'")
+    print(f"cfgfile = '{cfgfile}'")
+    print("---")
     hashes = {}
 
     class EventHandler(FileSystemEventHandler):
@@ -73,13 +122,13 @@ def serve(
             else:
                 hashes.pop(event.src_path, None)
             print(
-                "\033[1m - Change detected: '{}' {}.\033[m".format(
+                "\033[1m - Change detected: '{}' {}. Regenerating.\033[m".format(
                     event.src_path, event.event_type
                 )
             )
-            generate(srcdir, tpldir, distdir, cfgfile, globals)
+            with redirect_stdout(open(os.devnull, "w")):
+                generate(srcdir, tpldir, distdir, cfgfile, globals)
 
-    generate(srcdir, tpldir, distdir, cfgfile, globals)
     event_handler = EventHandler()
     observer = Observer()
     observer.schedule(event_handler, srcdir, recursive=True)
@@ -100,6 +149,9 @@ def serve(
                 "\033[31;1m - This is a development server. "
                 + "Do not use it in a production deployment.\033[m"
             )
+            with redirect_stdout(open(os.devnull, "w")):
+                generate(srcdir, tpldir, distdir, cfgfile, globals)
+            print("\033[1m - Generated.\033[m")
             httpd.serve_forever()
     except KeyboardInterrupt:
         print("\n\033[1m - Exiting.\033[m")
